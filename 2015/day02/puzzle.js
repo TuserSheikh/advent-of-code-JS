fb2f61a98ae73cb9356b98cf1ae4df83105bd3cc
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2021-11-30 12:28:04 +0600

    2015 day 2 solved

--- 2015/day02/puzzle.js
+++ 2015/day02/puzzle.js
@@ -1,15 +1,43 @@
 import { readFileSync } from 'fs';
 
 const partOne = () => {
-  const input = readFileSync('./2015/day02/input.txt').toString();
+  let input = readFileSync('./2015/day02/input.txt')
+    .toString()
+    .split('\n')
+    .slice(0, -1);
 
-  return input;
+  let result = 0;
+
+  for (let dimension of input) {
+    let [h, w, l] = dimension
+      .split('x')
+      .map(n => +n)
+      .sort((a, b) => a - b);
+
+    result += h * w + 2 * l * w + 2 * w * h + 2 * h * l;
+  }
+
+  return result;
 };
 
 const partTwo = () => {
-  const input = readFileSync('./2015/day02/input.txt').toString();
+  let input = readFileSync('./2015/day02/input.txt')
+    .toString()
+    .split('\n')
+    .slice(0, -1);
 
-  return input;
+  let result = 0;
+
+  for (let dimension of input) {
+    let [h, w, l] = dimension
+      .split('x')
+      .map(n => +n)
+      .sort((a, b) => a - b);
+
+    result += h + h + w + w + h * w * l;
+  }
+
+  return result;
 };
 
 export { partOne, partTwo };
